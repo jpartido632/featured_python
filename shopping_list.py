bd7ad37f96ecd541e0167a0c463759c3a736d81f
JPartido <132030695+jpartido632@users.noreply.github.com>
2024-07-31 19:03:02 +0800

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -1,36 +1,38 @@
-# A PROGRAM THAT LETS YOU 'add', 'remove ', 'mark' ITEMS as purchased
-
-grocery_list = []
+# A PROGRAM THAT LETS YOU 'add', 'remove' items as purchased
 
 def create_list():
-    print("A list has been created to track your shopping list!")
+    shopping_list = []
+    size_of_list = len(shopping_list)
 
+    if size_of_list < 1:
+        print("A list has been created to track your shopping list!")
+        return shopping_list
+    else:
+        print(shopping_list)
 
-def add():
+def add(shopping_list):
     while True:
         user_input = input("type the item you would like to add: ")
         try:
             item = str(user_input)
             print(f"you included an item: {item}")
-            grocery_list.append(item)   # include item at last index
+            shopping_list.append(item)   # include item at last index
             break
         except ValueError:
             print(f"the {item} is invalid, please try again.")
 
-def remove():
+def remove(shopping_list):
     while True:
         user_input = input("type the item you would like to remove: ")
         try:
             item = str(user_input)
             print(f"you removed an item: {item}")
-            grocery_list.remove(item)   # search and remove an item
+            shopping_list.remove(item)   # search and remove an item
             break
         except ValueError:
             print(f"the {item} is either not on the list, please try again.")
 
 
-print(grocery_list)
-add()
-print(grocery_list)
-remove()
-print(grocery_list)
+my_list = create_list()
+add(my_list)
+remove(my_list)
